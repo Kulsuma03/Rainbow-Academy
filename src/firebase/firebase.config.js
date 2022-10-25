1758b6d75578dff4e77e31ab5eb09f136404f6e7
Kulsuma Akter <kulsumams72@gmail.com>
2022-10-25 19:53:27 +0600

    firebase config added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZXcYhLRtc0JEnhoybCl86yRQjgeM4aJk",
+  authDomain: "rainbow-academy-50e27.firebaseapp.com",
+  projectId: "rainbow-academy-50e27",
+  storageBucket: "rainbow-academy-50e27.appspot.com",
+  messagingSenderId: "852582412586",
+  appId: "1:852582412586:web:9db907fd6380bb7fd7ee53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
